01754c6acdf9fe567fd0c0b88bd7140567971ded
Yosuke Sugishita <yosuke.sugi@gmail.com>
2013-12-03 11:12:14 -0800

    Cleaned first_n_lines.py (changed the target file name from train_small to train_head)

--- script/first_n_lines.py
+++ script/first_n_lines.py
@@ -1,14 +1,14 @@
-## Take first n lines from the 16GB train data and write them into a single file caled 'train_small'
+## Take first n lines from the 16GB train data and write them into a single file caled 'train_head'
 
 # The number of lines to take:
-n = 1000000 # There are about 160 million lines in the train file.
+n = 10**3 # There are about 160 million lines in the train file.
 
 import os
 this_file_path = os.path.realpath(__file__) # this file's path
 home_dir = os.path.dirname(os.path.dirname(this_file_path))
 data_dir = os.path.join(home_dir, "data")
 train_data = open(os.path.join(data_dir, "train"), 'rb')
-new_file_path = os.path.join(data_dir, "train_small")
+new_file_path = os.path.join(data_dir, "train_head")
 
 with open(new_file_path, 'w') as f:
     for i in range(0, n):
