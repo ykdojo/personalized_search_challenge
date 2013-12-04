8be873ebc05842a6966253a3e1e7dd4f33b45ef9
Yosuke Sugishita <yosuke.sugi@gmail.com>
2013-12-04 15:43:04 -0800

    Made count_lines_in_train.py, which counts the number of lines in train

---
+++ script/count_lines_in_train.py
@@ -0,0 +1,35 @@
+## Count the number of lines in the train file.
+#
+# It's useful for later deveopment, and this file can be
+#  used as a template for traversing all the lines in the
+#  train file.
+#
+# In Unix, one can use the following command to benchmark
+#  the time it takes to run this program.
+#  > time python count_lins_in_train.py
+#
+# Question: How about in Windows?
+
+
+import os
+this_file_path = os.path.realpath(__file__) # this file's path
+home_dir = os.path.dirname(os.path.dirname(this_file_path))
+data_dir = os.path.join(home_dir, "data")
+# Use small data (head) for testing-- use first_n_lines.py
+# train_data = open(os.path.join(data_dir, "train_head"), 'rb')
+train_data = open(os.path.join(data_dir, "train"), 'rb') # this one is the real data
+
+count = 0
+while True:
+    try:
+        # Print at every 10-millionth line
+        if count % (10 ** 7) == 0:
+            print "...reading the {0}th line".format(count)
+        # next() raises the StopIteration exeption when hitting the end
+        train_data.next()
+        count += 1
+    except StopIteration as e:
+        print "Reached the end of the file."
+        break
+
+print "The number of lines in the train file was:", count
