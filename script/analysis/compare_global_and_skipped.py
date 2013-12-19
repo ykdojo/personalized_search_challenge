6df3df944d9bd2898e647e56df17535bdb085251
Yosuke Sugishita <yosuke.sugi@gmail.com>
2013-12-18 23:16:13 -0800

    Put data into CSV files

--- script/analysis/compare_global_and_skipped.py
+++ script/analysis/compare_global_and_skipped.py
@@ -1,45 +1,28 @@
 ## This script compares the global means and skipped means for each rank position.
-# The numbers will be hard-coded here.
+# The numbers are from CSV files in data/results
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
+this_file_path = os.path.realpath(__file__) # this file's path
+home_dir = os.path.dirname(os.path.dirname(os.path.dirname(this_file_path)))
 
-### The following results are from 1% sample.
-# TODO: replace the following results with real results from 100% train.
+global_table = pd.read_csv(home_dir + '/data/results/global_means.csv')
+global_means = global_table['global_means']
+global_lengths = global_table['global_lengths']
 
-## Results, global case
-# Summary on relevance rates:
-# mean: [ 0.65629868  0.21159365  0.13479269  0.09191065  0.06907311  0.05177313
-#  0.04077629  0.03368095  0.03000333  0.03060453]
-# length: 638729
+skipped_table = pd.read_csv(home_dir + '/data/results/skipped_means.csv')
+skipped_means = skipped_table['skipped_means']
+skipped_lengths = skipped_table['skipped_lengths']
 
-## Results, skipped case
-# Sums:  [ 4725  3107  2325  1536  1135   845   645   497   427   254]
-# Lengths:  [ 16973  31691  32216  30529  29488  28075  26473  25400  22553   4343]
-# Means:  [ 0.27838331  0.09804045  0.07216911  0.05031282  0.03849023  0.03009795
-#  0.02436445  0.01956693  0.01893318  0.05848492]
+ratio_skipped = sum(skipped_lengths) / float(sum(global_lengths))
 
-
-### The following results are from the whole train file.
-# mean: [ 0.65162438  0.21038608  0.13324812  0.09167282  0.06874233  0.05194465
-#  0.04036323  0.0332495   0.02966097  0.03055541]
-# length: 65172853
-
-global_means = np.array([0.65629868,0.21159365,0.13479269,0.09191065,0.06907311,0.05177313,0.04077629,0.03368095,0.03000333,0.03060453])
-num_queries = 638729 # length: 638729
-num_hits = num_queries * 10
-skipped_means = np.array([0.27838331,0.09804045,0.07216911,0.05031282,0.03849023,0.03009795,0.02436445,0.01956693,0.01893318,0.05848492])
-# numbers of skipped urls that have been shown to users
-skipped_lengths = np.array([16973,31691,32216,30529,29488,28075,26473,25400,22553,4343])
-ratio_skipped = sum(skipped_lengths) / float(num_hits)
-
-global_sums = global_means * num_queries
+global_sums = global_means * global_lengths
 skipped_sums = skipped_means * skipped_lengths
 
-# numbers of non-skipped urls
-non_skipped_lengths = num_queries - skipped_lengths
+non_skipped_lengths = global_lengths - skipped_lengths
 non_skipped_means = (global_sums - skipped_sums) / non_skipped_lengths
-
 
 print "ratio_skipped:", ratio_skipped
 width = 0.2
