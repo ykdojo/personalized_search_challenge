38f8a5868c8af20b779bf0a79f823bfe252d2844
Yosuke Sugishita <yosuke.sugi@gmail.com>
2013-12-26 14:12:40 -0800

    More pseudo-code

--- script/analysis/liked_in_different_sessions.py
+++ script/analysis/liked_in_different_sessions.py
@@ -4,14 +4,23 @@
 # Look at query-document pairs users have liked in past sessions.
 
 # <Pseudo-code>
+## (in User class)
+# returns the dictionary of query-url pair that are rated 2.
+#   the form is: {(query_1, url_4): session_1, session_3, session_4, (query_2, url_3): session1}
+# the sessions are the ones in which each query-url pair has been rated 2.
+def get_queries_rated_2(self):
+    # if we already have this in the object, return it.
+    if not queries_rated_2 == None:
+        return queries_rated_2
+    # otherwise, compute it
+    else:
+        # find and set queries_rated_2
 
-# (in User class)
 # returns the number of times the user has liked the same query-document pair
 # in past sessions
-def times_liked_in_past_sessions(self, query, document):
-    get all the past queries
-    get the ones that are the same as the given query
-    
+def num_times_rated_2(self, query, url):
+    # use get_queries_rated_2 for this
+
 
 
 for each user:
